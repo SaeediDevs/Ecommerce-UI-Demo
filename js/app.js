7e999db6a3e2b966149e81ab7723a2c63b61fe1f
Alireza Saeedi <bf3alireza@gmail.com>
2025-08-28 20:19:25 +0330

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,14 +1,30 @@
-// setup nav
-const navBtn = document.getElementById("nav-btn");
-const navbar = document.getElementById("navbar");
-const navClose = document.getElementById("nav-close");
-
-// show nav
+// ********** nav toggle ************
+// select button and links
+const navBtn = document.getElementById("nav-toggle");
+const links = document.getElementById("nav-links");
+// add event listener
 navBtn.addEventListener("click", () => {
-  navbar.classList.add("showNav");
+    links.classList.toggle("show-links");
 });
 
-// close nav
-navClose.addEventListener("click", () => {
-  navbar.classList.remove("showNav");
+// ********** smooth scroll ************
+// select links
+const scrollLinks = document.querySelectorAll(".scroll-link");
+scrollLinks.forEach(link => {
+    link.addEventListener("click", e => {
+        // prevent default
+        e.preventDefault();
+        links.classList.remove("show-links");
+
+        const id = e.target.getAttribute("href").slice(1);
+        const element = document.getElementById(id);
+
+        let position = element.offsetTop - 62;
+
+        window.scrollTo({
+            left: 0,
+            top: position,
+            behavior: "smooth"
+        });
+    });
 });
